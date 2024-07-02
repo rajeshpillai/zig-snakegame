70dd3b34fe61d51ee5b7242ba15d8c54295ef694
Rajesh Pillai <rajesh.algorisys@ext.idfy.com>
2024-07-02 17:04:00 +0530

    Replace for with while due to u32 issue(need to figure out) and mod instead of %)

--- src/main.zig
+++ src/main.zig
@@ -37,7 +37,42 @@ var counter_tail: i32 = 0;
 fn update_game() void {
     if (!game_over) {
         if (rl.isKeyPressed(rl.KeyboardKey.key_p)) pause = !pause;
-        if (!pause) {}
+        if (!pause) {
+            if (rl.isKeyPressed(rl.KeyboardKey.key_right) and (snake[0].speed.x == 0) and allow_move) {
+                snake[0].speed = rl.Vector2.init(SQUARE_SIZE, 0);
+                allow_move = false;
+            }
+            if (rl.isKeyPressed(rl.KeyboardKey.key_left) and (snake[0].speed.x == 0) and allow_move) {
+                snake[0].speed = rl.Vector2.init(-SQUARE_SIZE, 0);
+                allow_move = false;
+            }
+            if (rl.isKeyPressed(rl.KeyboardKey.key_up) and (snake[0].speed.y == 0) and allow_move) {
+                snake[0].speed = rl.Vector2.init(0, -SQUARE_SIZE);
+                allow_move = false;
+            }
+            if (rl.isKeyPressed(rl.KeyboardKey.key_down) and (snake[0].speed.y == 0) and allow_move) {
+                snake[0].speed = rl.Vector2.init(0, SQUARE_SIZE);
+                allow_move = false;
+            }
+
+            var j: u32 = 0;
+            while (j < counter_tail) : (j += 1) {
+                snake_position[j] = snake[j].position;
+            }
+
+            if (@mod(frames_counter, 5) == 0) {
+                var i: u32 = 0;
+                while (i < counter_tail) : (i += 1) {
+                    if (i == 0) {
+                        snake[0].position.x += snake[0].speed.x;
+                        snake[0].position.y += snake[0].speed.y;
+                        allow_move = true;
+                    } else {
+                        snake[i].position = snake_position[i - 1];
+                    }
+                }
+            }
+        }
         const pause_text = if (pause) "PAUSED" else "RESUMED";
         rl.drawText(pause_text, 10, 10, 32, rl.Color.yellow);
     }
